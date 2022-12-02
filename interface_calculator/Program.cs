bb40f7b7d4befba0592fa011d83759a6ba9e59cf
nandagopal.rn <nandagopal.rn@PIA3278>
2022-12-02 15:09:37 +0530

    Add project files.

---
+++ interface_calculator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace interface_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    Calculator\n");
+
+            Calculator ModernCalculator = new Calculator();
+
+            IBasic NormalCalculator = new Calculator();
+
+            Console.WriteLine(" Calculator Types\n\n1.Normal Calculator\n2.Scientific Calculator");
+            Console.WriteLine("\nSelect your Calculator Type ? ");
+            int CalculatorType = int.Parse(Console.ReadLine());
+            switch (CalculatorType)
+            {
+                case 2:
+
+                    ModernCalculator.TakeUserInput();
+
+                    Console.WriteLine($"1.Addition\n2.Substraction\n3.Multiplication\n4.Division");
+                    Console.WriteLine($"5.Matrix Operation\n6.Log Operations\n7.Exponential");
+                    Console.WriteLine("\nEnter your choice");
+                    int FirstChoice = int.Parse(Console.ReadLine());
+
+                    switch (FirstChoice)
+                    {
+                        case 1:
+                            ModernCalculator.Addition();
+                            break;
+                        case 2:
+                            ModernCalculator.Substraction();
+                            break;
+                        case 3:
+                            ModernCalculator.Multiplication();
+                            break;
+                        case 4:
+                            ModernCalculator.Division();
+                            break;
+                        case 5:
+                            ModernCalculator.MatrixOperation();
+                            break;
+                        case 6:
+                            ModernCalculator.LogOperations();
+                            break;
+                        case 7:
+                            ModernCalculator.Exponential();
+                            break;
+                        default:
+                            Console.WriteLine("Enter a valid choice");
+                            break;
+                    }
+                    break;
+                case 1:
+
+                    NormalCalculator.TakeUserInput();
+
+                    Console.WriteLine($"1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n");
+                    Console.WriteLine("\nEnter your choice");
+                    int SecondChoice = int.Parse(Console.ReadLine());
+
+                    switch (SecondChoice)
+                    {
+                        case 1:
+                            NormalCalculator.Addition();
+                            break;
+                        case 2:
+                            NormalCalculator.Substraction();
+                            break;
+                        case 3:
+                            NormalCalculator.Multiplication();
+                            break;
+                        case 4:
+                            NormalCalculator.Division();
+                            break;
+                        default:
+                            Console.WriteLine("Enter a valid choice");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid type");
+                    break;
+            }
+        }
+    }
+}
---
+++ interface_calculator/calculatorInterface.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace interface_calculator
+{
+    interface IBasic
+    {
+        void TakeUserInput();
+
+        void Addition();
+
+        void Substraction();
+
+        void Multiplication();
+
+        void Division();
+    }
+
+    interface IScientific
+    {
+        void MatrixOperation();
+
+        void LogOperations();
+
+        void Exponential();
+    }
+
+    class Calculator : IBasic, IScientific
+    {
+
+        public float FirstNumber;
+        public float SecondNumber;
+
+        public void TakeUserInput()
+        {
+            Console.WriteLine("Enter the First Number:");
+            FirstNumber = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Number:");
+            SecondNumber = float.Parse(Console.ReadLine());
+        }
+        
+        public void Addition()
+        {
+            Console.WriteLine($"The sum is {FirstNumber + SecondNumber}");
+        }
+
+        public void Division()
+        {
+            Console.WriteLine($"The Quotient is {FirstNumber / SecondNumber}");
+        }
+
+        public void Exponential()
+        {
+            Console.WriteLine("The Exponential Operation in Scientific is done");
+        }
+
+        public void LogOperations()
+        {
+            Console.WriteLine("The Log Operation in Scientific is done");
+        }
+
+        public void MatrixOperation()
+        {
+            Console.WriteLine("The Matrix Operation in Scientific is done");
+        }
+
+        public void Multiplication()
+        {
+            Console.WriteLine($"The Product is {FirstNumber * SecondNumber}");
+        }
+
+        public void Substraction()
+        {
+            Console.WriteLine($"The Difference is {FirstNumber - SecondNumber}");
+        }
+    }
+}
